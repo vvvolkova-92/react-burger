4e746fa358977d0de2db1feb9c39ee6c10e7dee1
Vilo4ka <saiviolet@gmail.com>
2022-01-26 23:11:36 +0300

    Подключение к API

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 import styles from './App.module.css';
 const BASEURL = 'https://norma.nomoreparties.space/api/ingredients';
 function App() {
-
+  // 2. Подключитесь к API
   const getData = async (url: string) => {
     const res = await fetch(url);
     if (res.ok) return res.json();
