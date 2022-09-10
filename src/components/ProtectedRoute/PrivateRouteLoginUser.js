a9468d25fd5d8d7605cc5505843f68a047ce0964
Vilo4ka <saiviolet@gmail.com>
2022-09-10 17:27:36 +0300

    Редирект в приватном роуте

--- src/components/ProtectedRoute/PrivateRouteLoginUser.js
+++ src/components/ProtectedRoute/PrivateRouteLoginUser.js
@@ -5,12 +5,11 @@ import { useLocation } from "react-router-dom";
 function PrivateRouteLoginUser({ children, ...rest }) {
   const isLogin = useSelector((state) => state.userReducer.isLogin);
   const location = useLocation();
-  let { from } = location.state || { from: { pathname: "/" } };
   return (
     <Route
       {...rest}
       render={() =>
-        !isLogin ? children : <Redirect to={location ? location.state.from : "/"} />
+        !isLogin ? children : <Redirect to={location?.state?.from || "/"} />
       }
     />
   );
